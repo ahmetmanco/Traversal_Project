7f11146e3e2911747f0e67eb8c0aa4603c00664f
Ahmet Gündüz <ahmetmanco07@gmail.com>
2024-01-04 18:24:17 +0300

    update

--- Traversal/Areas/Admin/Controllers/DestinationController.cs
+++ Traversal/Areas/Admin/Controllers/DestinationController.cs
@@ -1,13 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EF;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Traversal.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EFDestinationDal()); 
         public IActionResult Index()
         {
+            var value = destinationManager.TGetList();
+            return View(value);
+        }
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteDestination(int id)
+        {
+            var value = destinationManager.TGetById(id);
+            destinationManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateDestination(int id)
+        {
+            var value = destinationManager.TGetById(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index");
         }
     }
 }
