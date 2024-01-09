301d47e838bda58de42908993ce36beeaca877aa
Ahmet Gündüz <ahmetmanco07@gmail.com>
2024-01-09 13:28:58 +0300

    insert

---
+++ Traversal/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Traversal.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(Cityes);
+            return View(jsonCity);
+        }
+        public static List<CityClass> Cityes = new List<CityClass>()
+        { new CityClass()
+            {
+            Id = 1,
+            CityName = "Üsküp",
+            CityCountry = "Makedonya"
+            },
+            new CityClass()
+            {
+            Id = 2,
+            CityName = "Roma",
+            CityCountry = "İtalya"
+            },
+            new CityClass()
+            {
+            Id = 3,
+            CityName = "Londra",
+            CityCountry = "İngiltere"
+            }
+        };
+    }
+}
---
+++ Traversal/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,46 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+//using System.Net.Mail;
+using Traversal.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "traversal22@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom); // mailin kimden geldiği
+
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo); // mailin kime gideceği
+
+            var bodybuilder = new BodyBuilder();
+            bodybuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodybuilder.ToMessageBody(); // Mailin içeriği
+
+            //mimeMessage.Body = mailRequest.Body.
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient Client = new SmtpClient();
+            Client.Connect("smtp.gmail.com", 587, false);
+
+            Client.Authenticate("traversal22@gmail.com", "zobc kcom znct ixlk"/*şifresi*/);
+
+            Client.Send(mimeMessage);
+            Client.Disconnect(true);
+            return View();
+        }
+    }//traversal22@gmail.com
+}
