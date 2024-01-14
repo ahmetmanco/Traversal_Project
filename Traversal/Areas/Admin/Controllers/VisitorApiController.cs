a224a7f1c4a19dec07de6ff160bca6649c9b58ac
Ahmet Gündüz <ahmetmanco07@gmail.com>
2024-01-14 21:56:28 +0300

    update

---
+++ Traversal/Areas/Admin/Controllers/VisitorApiController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using Traversal.Areas.Admin.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class VisitorApiController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public VisitorApiController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            if (_httpClientFactory == null)
+            {
+                // _httpClientFactory null ise uygun bir şekilde enjekte edilmemiş demektir.
+                // Hata durumunu belirleyin ve işleme alın
+                ViewBag.ErrorMessage = "IHttpClientFactory uygun bir şekilde enjekte edilmemiş.";
+                return View();
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5016/api/Visitor");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+                if (!string.IsNullOrEmpty(jsonData))
+                {
+                    var value = JsonConvert.DeserializeObject<List<VisitorViewModel>>(jsonData);
+                    return View(value);
+                }
+            }
+
+            // Hata durumunu belirleyin
+            // Örneğin: ViewBag.ErrorMessage = $"API çağrısı başarısız. Durum Kodu: {responseMessage.StatusCode}";
+
+            // Hata mesajını kontrol edin ve işleme alın
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddVisitor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddVisitor(VisitorViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5016/api/Visitor", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public async Task<IActionResult> DeleteVisitor(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5016/api/Visitor/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateVisitor(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5016/api/Visitor/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<VisitorViewModel>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateVisitor(VisitorViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var JsonData = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(JsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PutAsync("http://localhost:5016/api/Visitor", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
