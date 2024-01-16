ceeab094b91994ad7483f39682b55cf998c83948
Ahmet Gündüz <ahmetmanco07@gmail.com>
2024-01-16 22:27:16 +0300

    update

---
+++ Traversal/Areas/Admin/Controllers/ApiExchangesController.cs
@@ -0,0 +1,38 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using Traversal.Areas.Admin.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    public class ApiExchangesController : Controller
+    {
+        [Area("Admin")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            List<BookingExcModel2> models = new List<BookingExcModel2>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=AED&locale=en-gb"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "aaca464d2bmshaa35f81362e2898p1942b2jsn36c22d5af144" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<BookingExcModel2>(body);
+                return View(value.exchange_rates);
+            }
+
+        }
+    }
+}
