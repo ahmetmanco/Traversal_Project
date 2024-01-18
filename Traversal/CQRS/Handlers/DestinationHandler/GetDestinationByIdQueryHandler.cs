0b87cf4e92f646da2116f1901e5f66b8ecee53b4
Ahmet Gündüz <ahmetmanco07@gmail.com>
2024-01-18 14:18:31 +0300

    update

---
+++ Traversal/CQRS/Handlers/DestinationHandler/GetDestinationByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using DataAccessLayer.Concrete;
+using Traversal.CQRS.Queries.DestinationQuery;
+using Traversal.CQRS.Results.DestinationResult;
+
+namespace Traversal.CQRS.Handlers.DestinationHandler
+{
+    public class GetDestinationByIdQueryHandler
+    {
+        private readonly Context _context;
+
+        public GetDestinationByIdQueryHandler(Context context)
+        {
+            _context = context;
+        }
+
+        public GetDestinationByIdQueryResult Handle(GetDestinationByIdQuery query) 
+        {
+            var value = _context.Destinationss.Find(query.Id);
+            return new GetDestinationByIdQueryResult
+            {
+                DestinationId = value.Id,
+                City = value.City,
+                DayNight = value.DayNight
+            };
+        }
+    }
+}
