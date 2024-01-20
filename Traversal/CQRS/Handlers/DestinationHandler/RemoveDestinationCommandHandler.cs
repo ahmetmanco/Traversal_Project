4b783ee8668f0a8099ec6f3c48e50af451f451c1
Ahmet Gündüz <ahmetmanco07@gmail.com>
2024-01-20 20:50:05 +0300

    update

---
+++ Traversal/CQRS/Handlers/DestinationHandler/RemoveDestinationCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Concrete;
+using Traversal.CQRS.Commands.DestinationCommands;
+
+namespace Traversal.CQRS.Handlers.DestinationHandler
+{
+    public class RemoveDestinationCommandHandler
+    {
+        private readonly Context _context;
+
+        public RemoveDestinationCommandHandler(Context context)
+        {
+            _context = context;
+        }
+        public void Handle(RemoveDestinationCommand command)
+        {
+            var value = _context.Destinationss.Find(command.Id);
+            _context.Destinationss.Remove(value);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Traversal/CQRS/Handlers/DestinationHandler/UpdateDestinationCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Concrete;
+using Traversal.CQRS.Commands.DestinationCommands;
+
+namespace Traversal.CQRS.Handlers.DestinationHandler
+{
+    public class UpdateDestinationCommandHandler
+    {
+        private readonly Context _context;
+
+        public UpdateDestinationCommandHandler(Context context)
+        {
+            _context = context;
+        }
+
+        public void Handle(UpdateDestinationCommands commandHandler)
+        {
+            var value = _context.Destinationss.Find(commandHandler.Id);
+            value.City = commandHandler.City;
+            value.DayNight = commandHandler.DayNight;
+            value.Price = commandHandler.Price;
+            value.Capacity = commandHandler.Capacity;
+
+            _context.SaveChanges();
+        }
+    }
+}
