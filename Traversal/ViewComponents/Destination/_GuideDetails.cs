2fa0a591182a895793b8584dff07fa82028c53e7
Ahmet Gündüz <ahmetmanco07@gmail.com>
2024-02-07 01:00:02 +0300

    update

---
+++ Traversal/ViewComponents/Destination/_GuideDetails.cs
@@ -0,0 +1,21 @@
+﻿using BussinessLayer.Abstract;
+using BussinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.ViewComponents.Destination
+{
+    public class _GuideDetails : ViewComponent
+    {
+        private readonly IGuideService _guideService;
+
+        public _GuideDetails(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+        public IViewComponentResult Invoke(int id)
+        {
+            var value = _guideService.TGetById(id);
+            return View(value);
+        }
+    }
+}
